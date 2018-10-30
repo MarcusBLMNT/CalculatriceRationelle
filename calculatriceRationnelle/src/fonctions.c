568cf1dcea1a6bfcc979739b13d84e0086f004a8
Marcus BELMONT <marcus.belmont@viacesi.fr>
2018-10-30 21:54:48 +0100

    Add files via upload

---
+++ calculatriceRationnelle/src/fonctions.c
@@ -0,0 +1,8 @@
+/*
+ * fonctions.c
+ *
+ *  Created on: 30 oct. 2018
+ *      Author: Marcus
+ */
+
+
---
+++ calculatriceRationnelle/src/header.h
@@ -0,0 +1,13 @@
+/*
+ * header.h
+ *
+ *  Created on: 30 oct. 2018
+ *      Author: Marcus
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+
+
+#endif /* HEADER_H_ */
